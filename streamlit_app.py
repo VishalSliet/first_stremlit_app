3998e68c6a55922a015fdce09c6b248730014b53
Vishal Kumar <134846666+VishalSliet@users.noreply.github.com>
2024-01-28 21:46:07 +0530

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,30 +8,11 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Advaco')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-# Display the table on the page.
-my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.dataframe(my_fruit_list)
-
-
-streamlit.multiselect("pick some fruit:", list(my_fruit_list.index))
-
-fruit_selected = streamlit.multiselect("pick some fruit:", list(my_fruit_list.index),["Avocado","Apple"])
-fruit_to_show = my_fruit_list.loc[fruit_selected]
-streamlit.dataframe(fruit_to_show)
-
-
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
