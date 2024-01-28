575aa6a2c3130922e489a5bf3e66ca70769b86b1
Vishal Kumar <134846666+VishalSliet@users.noreply.github.com>
2024-01-28 21:43:25 +0530

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,5 +30,7 @@ streamlit.dataframe(fruit_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 
 
